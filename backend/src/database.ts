c596150aaa1d18cc7f53bc9630b8b93d8adfa6a6
spotup-9052 <spotup@gmail.com>
2025-10-15 16:01:56 +0200

    Fix database initialization: add back created and updated columns to sysop INSERT

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1274,14 +1274,14 @@ export class Database {
           timeTotal, timeLimit, timeUsed, chatLimit, chatUsed, firstLogin,
           calls, callsToday, newUser, expert, ansi, linesPerScreen, computer,
           screenType, protocol, editor, zoomType, availableForChat, quietNode,
-          autoRejoin, confAccess, areaName, uuCP, topUploadCPS, topDownloadCPS, byteLimit
-        ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30, $31, $32, $33, $34, $35, $36, $37, $38, $39, $40, $41)
+          autoRejoin, confAccess, areaName, uuCP, topUploadCPS, topDownloadCPS, byteLimit, created, updated
+        ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30, $31, $32, $33, $34, $35, $36, $37, $38, $39, $40, $41, $42, $43)
         ON CONFLICT (username) DO NOTHING
       `, [
         'sysop-user-id', 'sysop', hashedPassword, 'System Operator', 'Server Room', '', '',
         255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, new Date(),
         0, 0, false, true, true, 23, 'Server', 'Amiga Ansi', '/X Zmodem', 'Prompt',
-        'QWK', true, false, 1, 'XXX', 'Sysop', false, 0, 0, 0
+        'QWK', true, false, 1, 'XXX', 'Sysop', false, 0, 0, 0, new Date(), new Date()
       ]);
 
       console.log('Default data initialization completed');
