fe6a5ec8844156566d0fd5fab2858b850529bb22
spotup-9052 <spotup@gmail.com>
2025-10-15 16:11:01 +0200

    Fix VALUES clause - remove extra placeholder to match 40 columns

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1275,7 +1275,7 @@ export class Database {
           calls, callsToday, newUser, expert, ansi, linesPerScreen, computer,
           screenType, protocol, editor, zoomType, availableForChat, quietNode,
           autoRejoin, confAccess, areaName, uuCP, topUploadCPS, topDownloadCPS, byteLimit
-        ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30, $31, $32, $33, $34, $35, $36, $37, $38, $39, $40, $41)
+        ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30, $31, $32, $33, $34, $35, $36, $37, $38, $39, $40)
         ON CONFLICT (username) DO NOTHING
       `, [
         'sysop-user-id', 'sysop', hashedPassword, 'System Operator', 'Server Room', '', '',
